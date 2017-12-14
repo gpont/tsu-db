69bbb004f80f973e38f33744085c9697c03ef792
Evgeniy Guzhikhin <gpont97@gmail.com>
2017-12-14 22:09:18 +0700

    refactor, fix some errors (lab4)

--- src/lab4.sql
+++ src/lab4.sql
@@ -1,149 +1,123 @@
--- 1. Написать триггер, активизирующийся при изменении содержимого таблицы `Orders` и проверяющий, чтобы срок доставки был больше текущего времени не менее чем на 30 минут. Если время заказа не указано автоматически должно проставляться текущее время, если срок доставки не указан, то он автоматически должен ставиться на час позже времени заказа.
+-- 1. Написать триггер, активизирующийся при изменении содержимого таблицы `Orders` и проверяющий, чтобы срок доставки
+-- был больше текущего времени не менее чем на 30 минут. Если время заказа не указано автоматически должно проставляться
+-- текущее время, если срок доставки не указан, то он автоматически должен ставиться на час позже времени заказа.
 
--- instead of are only for views and cannot be used with tables
-create or replace view Order_View as select * from Orders;
+-- drop old objects for tests
+drop trigger ORDERS_TRIGGER;
 
-create or replace trigger Delivery
-instead of insert on Order_View
-for each row
-begin
-    :new.order_date := nvl(:new.order_date, sysdate); -- return second arg if first is null
-    :new.delivery_date := nvl(:new.delivery_date, to_date(sysdate + 1/24));
-/*
-    if (:new.order_date is null) then
-        :new.order_date := sysdate;
+-- "instead of" are only for views and cannot be used with tables
+create or replace view ORDERS_VIEW as
+  select *
+  from ORDERS;
+
+create or replace trigger DELIVERY_TRIGGER
+  before insert
+  on ORDERS
+  for each row
+  begin
+    :NEW.ORDER_DATE := nvl(:NEW.ORDER_DATE, sysdate); -- return second arg if first is null
+    :NEW.DELIVERY_DATE := nvl(:NEW.DELIVERY_DATE, to_date(sysdate + 1 / 24));
+    if (:NEW.DELIVERY_DATE - :NEW.ORDER_DATE) * 24 < 0.5
+    then
+      RAISE_APPLICATION_ERROR(
+        -20001,
+        'ERROR: too short delivery date'
+      );
     end if;
-    if (:new.delivery_date is null) then
-        :new.delivery_date := to_date(sysdate + 1/24);
-    end if;
-*/
-    if (new_delivery_date - new_order_date)*24 < 0.5 then
-        dbms_output.put_line('ERROR: too short delivery date');
-    else
-        insert into Orders (
-            order_id,
-            payment_method_id,
-            employee_id,
-            customer_id,
-            ex_comment,
-            location_id,
-            delivery_date,
-            order_date,
-            end_date
-        ) values (
-            :new.order_id,
-            :new.payment_method_id,
-            :new.employee_id,
-            :new.customer_id,
-            :new.ex_comment,
-            :new.location_id,
-            new_delivery_date,
-            new_order_date,
-            :new.end_date
-        );
-    end if;
-end;
-/
--- Tests
--- show errors trigger Delivery;
-/*
-insert into Orders (
-    order_id,
-    payment_method_id,
-    employee_id,
-    customer_id,
-    ex_comment,
-    location_id
-) values (
-    731,
-    1,
-    1,
-    1,
-    ' ',
-    1
+  end;
+
+-- 2. Написать триггер, сохраняющий статистику изменений таблицы `EMPLOYEES` в таблице (таблицу создать), в которой
+-- хранятся номер сотрудника дата изменения, тип изменения (insert, update, delete). Триггер также выводит на экран
+-- сообщение с указанием количества дней прошедших со дня последнего изменения.
+
+-- dropping old objects for tests
+drop sequence ID_ITER;
+drop table EMPLOYEES_STATS;
+
+create table EMPLOYEES_STATS (
+  ID          int not null primary key,
+  EMPLOYEE_ID int not null,
+  DATE_CHANGE date,
+  TYPE_CHANGE varchar2(10)
 );
 
-select *
-from Orders
-where order_id = 731;
-*/
--- 2. Написать триггер, сохраняющий статистику изменений таблицы `EMPLOYEES` в таблице (таблицу создать), в которой хранятся номер сотрудника дата изменения, тип изменения (insert, update, delete). Триггер также выводит на экран сообщение с указанием количества дней прошедших со дня последнего изменения.
+-- iterator for increment id field
+create sequence ID_ITER
+  minvalue 1
+  start with 1
+  increment by 1
+  cache 10;
 
--- create table Stats (
---     id int not null primary key,
---     employee_id int not null,
---     date_change date,
---     type_change varchar2(10)
--- );
+create or replace trigger EMPLOYEES_STATS_TRIGGER
+  after insert or update or delete
+  on EMPLOYEES
+  for each row
+  declare
+    NEW_TYPE_CHANGE varchar2(6);
+    LAST_CHANGE     date;
+  begin
+    select LAST_CHANGE = DATE_CHANGE
+    from EMPLOYEES
+    order by EMPLOYEES_PK
+    desc;
 
--- -- iterator for increment id field
--- create sequence id_iter
--- minvalue 1
--- start with 1
--- increment by 1
--- cache 10;
+    ID_ITER.NEXTVAL; -- increment id iterator
+    if inserting
+    then
+      NEW_TYPE_CHANGE := 'insert';
+    elsif updating
+    then
+      NEW_TYPE_CHANGE := 'update';
+    elsif deleting
+    then
+      NEW_TYPE_CHANGE := 'delete';
+    end if;
 
--- create trigger Stats_Trigger
--- after insert or update or delete on Employees
--- for each row
--- declare
---     new_type_change varchar2(10);
---     last_change date;
--- begin
---     select last_change = date_change
---     from employees
---     order by id
---     desc;
+    insert into STATS (
+      ID,
+      EMPLOYEE_ID,
+      DATE_CHANGE,
+      TYPE_CHANGE
+    ) values (
+      ID_ITER.CURVAL,
+      :NEW.EMPLOYEE_ID,
+      sysdate,
+      NEW_TYPE_CHANGE
+    );
 
---     id_iter.nextval; -- increment id iterator
---     case
---         when insert then
---             new_type_change := 'insert';
---         when delete then
---             new_type_change := 'delete';
---         when update then
---             new_type_change := 'update';
---     end case;
-
---     insert into stats (
---         id,
---         employee_id,
---         date_change,
---         type_change
---     ) values (
---         id_iter.curval,
---         :new.employee_id,
---         sysdate,
---         new_type_change
---     )
-
---     dbms_output.put_line('info: '||(days(sysdate)-days(last_change))||' days have passed since the last change');
--- end;
--- /
+    DBMS_OUTPUT.put_line('INFO: ' || (days(sysdate) - days(LAST_CHANGE)) || ' days have passed since the last change');
+  end;
 
 -- 3. Добавить к таблице `Orders` не обязательное поле `cipher`, которое должно заполняться автоматически согласно шаблону: `<YYYYMMDD> - <номер район> - <номер заказа в рамках месяца>`. Номера не обязательно должны соответствовать дате заказа, если район не известен, то “ номер района” равен 0.
 
--- alter table Orders
--- add cipher varchar2(24);
+dropping old objects for tests
+alter table ORDERS
+  drop column CIPHER;
 
--- create trigger Orders_Trigger
--- after insert on Orders
--- for each row
--- begin
---     declare cipher_str varchar2(24);
---     declare order_number int;
+alter table ORDERS
+  add CIPHER varchar2(24);
 
---     -- getting order number within the order month
---     select order_number = count(id), order_date
---     where extract(month from order_date) = extract(month from :new.order_date);
+create or replace trigger ORDERS_TRIGGER
+  after insert
+  on ORDERS
+  for each row
+  declare
+    CIPHER_STR   varchar2(24);
+    ORDER_NUMBER int;
+  begin
+    -- getting order number within the order month
+    select
+      ORDER_NUMBER = count(:NEW.ORDER_ID),
+      ORDER_DATE
+    from ORDERS
+    where extract( month from order_date) = extract( month from :new.order_date);
 
---     cipher_str = concat(
---         concat(to_char(sysdate, '<YYYYMMDD> - '), to_char(:new.location_id)),
---         concat(' - ', order_number)
---     );
+    CIPHER_STR = concat(
+        concat(to_char(sysdate, '<YYYYMMDD> - '), to_char(:NEW.LOCATION_ID)),
+        concat(' - ', ORDER_NUMBER)
+    );
 
---     update Orders
---     set cipher = cipher_str
---     where id = :new.id;
--- end;
--- /
+    update ORDERS
+    set CIPHER = CIPHER_STR
+    where ORDER_ID = :NEW.ORDER_ID;
+  end;
