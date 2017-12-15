0844e693e6870759c18fc27e3f513a2037842315
Evgeniy Guzhikhin <gpont97@gmail.com>
2017-12-15 18:30:55 +0700

    code styling, rm trash, a little fixes

--- src/lab4.sql
+++ src/lab4.sql
@@ -2,9 +2,6 @@
 -- был больше текущего времени не менее чем на 30 минут. Если время заказа не указано автоматически должно проставляться
 -- текущее время, если срок доставки не указан, то он автоматически должен ставиться на час позже времени заказа.
 
--- drop old objects for tests
-drop trigger ORDERS_TRIGGER;
-
 -- "instead of" are only for views and cannot be used with tables
 create or replace view ORDERS_VIEW as
   select *
@@ -20,8 +17,8 @@ create or replace trigger DELIVERY_TRIGGER
     if (:NEW.DELIVERY_DATE - :NEW.ORDER_DATE) * 24 < 0.5
     then
       RAISE_APPLICATION_ERROR(
-        -20001,
-        'ERROR: too short delivery date'
+          -20001,
+          'ERROR: too short delivery date'
       );
     end if;
   end;
@@ -31,55 +28,44 @@ create or replace trigger DELIVERY_TRIGGER
 -- сообщение с указанием количества дней прошедших со дня последнего изменения.
 
 -- dropping old objects for tests
-drop sequence ID_ITER;
 drop table EMPLOYEES_STATS;
 
 create table EMPLOYEES_STATS (
-  ID          int not null primary key,
   EMPLOYEE_ID int not null,
   DATE_CHANGE date,
   TYPE_CHANGE varchar2(10)
 );
-
--- iterator for increment id field
-create sequence ID_ITER
-  minvalue 1
-  start with 1
-  increment by 1
-  cache 10;
 
 create or replace trigger EMPLOYEES_STATS_TRIGGER
   after insert or update or delete
   on EMPLOYEES
   for each row
   declare
-    NEW_TYPE_CHANGE varchar2(6);
+    NEW_TYPE_CHANGE varchar2;
     LAST_CHANGE     date;
   begin
+    -- get last change date
     select LAST_CHANGE = DATE_CHANGE
-    from EMPLOYEES
-    order by EMPLOYEES_PK
+    from EMPLOYEES_STATS
+    order by DATE_CHANGE
     desc;
 
-    ID_ITER.NEXTVAL; -- increment id iterator
     if inserting
     then
       NEW_TYPE_CHANGE := 'insert';
     elsif updating
-    then
-      NEW_TYPE_CHANGE := 'update';
+      then
+        NEW_TYPE_CHANGE := 'update';
     elsif deleting
-    then
-      NEW_TYPE_CHANGE := 'delete';
+      then
+        NEW_TYPE_CHANGE := 'delete';
     end if;
 
-    insert into STATS (
-      ID,
+    insert into EMPLOYEES_STATS (
       EMPLOYEE_ID,
       DATE_CHANGE,
       TYPE_CHANGE
     ) values (
-      ID_ITER.CURVAL,
       :NEW.EMPLOYEE_ID,
       sysdate,
       NEW_TYPE_CHANGE
@@ -90,7 +76,7 @@ create or replace trigger EMPLOYEES_STATS_TRIGGER
 
 -- 3. Добавить к таблице `Orders` не обязательное поле `cipher`, которое должно заполняться автоматически согласно шаблону: `<YYYYMMDD> - <номер район> - <номер заказа в рамках месяца>`. Номера не обязательно должны соответствовать дате заказа, если район не известен, то “ номер района” равен 0.
 
-dropping old objects for tests
+-- dropping old objects for tests
 alter table ORDERS
   drop column CIPHER;
 
@@ -102,7 +88,7 @@ create or replace trigger ORDERS_TRIGGER
   on ORDERS
   for each row
   declare
-    CIPHER_STR   varchar2(24);
+    CIPHER_STR   varchar2;
     ORDER_NUMBER int;
   begin
     -- getting order number within the order month
@@ -110,7 +96,7 @@ create or replace trigger ORDERS_TRIGGER
       ORDER_NUMBER = count(:NEW.ORDER_ID),
       ORDER_DATE
     from ORDERS
-    where extract( month from order_date) = extract( month from :new.order_date);
+    where extract(month from ORDER_DATE) = extract(month from :NEW.ORDER_DATE);
 
     CIPHER_STR = concat(
         concat(to_char(sysdate, '<YYYYMMDD> - '), to_char(:NEW.LOCATION_ID)),
